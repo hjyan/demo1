73dc6b7dab68a313ae9064da01d5536c89442aed
Administrator <806639956@qq.com>
2018-08-09 21:57:59 +0800

    springbootdemo

---
+++ src/main/java/com/zyyan/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.zyyan.demo;
+
+import com.sun.org.apache.xpath.internal.operations.String;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class);
+    }
+}
+
+
