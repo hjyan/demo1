5aa138333473e35383a8e0094a1d15e907a1302b
Administrator <806639956@qq.com>
2018-08-09 22:22:28 +0800

    second method

--- src/main/java/com/zyyan/demo/DemoApplication.java
+++ src/main/java/com/zyyan/demo/DemoApplication.java
@@ -13,6 +13,6 @@ public class DemoApplication  implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("init... method");
+        System.out.println("init... method.....second");
     }
 }
